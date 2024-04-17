99f288058c58f2eb85b1e6a1c1997a82de8d37f8
AnaBarbancho <ana.afonso2@fatec.sp.gov.br>
2024-04-16 22:28:51 -0300

    .

---
+++ src/contexts/CMYKContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, useState } from "react";
+import { CMYKContextProps} from "../types";
+
+export const CMYKContext = createContext({} as CMYKContextProps);
+
+export function CMYKProvider({children}: any){
+    const [cyan, setCyan] = useState(0);
+    const [magenta, setMagenta] = useState(0);
+    const [yellow, setYellow] = useState(0);
+    const [black, setBlack] = useState(0);
+    const color = `cmyk(${cyan}, ${magenta}, ${yellow},${black})`;
+
+    return (
+        <CMYKContext.Provider
+            value={{
+                cyan,
+                magenta,
+                yellow,
+                black,
+                setCyan,
+                setMagenta,
+                setYellow,
+                setBlack,
+                color,
+            }}
+        >
+            {children}
+        </CMYKContext.Provider>
+    );
+}
